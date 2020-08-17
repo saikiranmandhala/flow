801294e6499b16520b565720edaa89ae92510a12
Sai Kiran Mandhala <saikiran.mandhala@gmail.com>
2020-08-17 17:29:22 +0530

    first commit

---
+++ WorkflowModule/IWorkflow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkflowModule
+{
+    public interface IWorkflow
+    {
+      
+        List<Transition> Transitions { get; }
+
+        Transition GetTransition(Transition transition);
+
+        Transition ExecuteTransition(Transition transition);
+
+    }
+    public class Transition
+    {
+        public string CurrentStatus { get; set; }
+        public string CurrentPhase { get; set; }
+        public string Action { get; set; }
+        public string Condition { get; set; }
+        public string ResultStatus { get; set; }
+        public string ResultPhase { get; set; }
+        public List<Role> Roles { get; set; }
+        public Object PostTransition { get; set; }
+    }
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleCode { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ WorkflowModule/Workflow.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkflowModule
+{
+    public class Workflow
+    {
+
+        public Workflow()
+        {
+            Transition newAcpCase = new Transition()
+            {
+                CurrentPhase = string.Empty,
+                CurrentStatus = string.Empty,
+                Action = "NewACP",
+                ResultPhase = "Intake",
+                ResultStatus = "PendingAssignment",
+                Condition = "ACP"
+            };
+            Transition newNonAcpCase = new Transition()
+            {
+                CurrentPhase = string.Empty,
+                CurrentStatus = string.Empty,
+                Action = "NewNonACP",
+                ResultPhase = "Intake",
+                ResultStatus = "Unassigned",
+                Condition = "NonACP"
+            };
+            Transition claimNonAcpCase = new Transition()
+            {
+                CurrentPhase = "Intake",
+                CurrentStatus = "Assigned",
+                Action = "Claim",
+                ResultPhase = "RootCause",
+                ResultStatus = "Assigned",
+                Condition = string.Empty
+            };
+            Transition claimAcpCase = new Transition()
+            {
+                CurrentPhase = "Intake",
+                CurrentStatus = "Pending Assignment",
+                Action = "Claim",
+                ResultPhase = "RootCause",
+                ResultStatus = "Assigned",
+                Condition = string.Empty
+            };
+        }
+
+        public List<Transition> Transitions { get; }
+
+        public void UpdateCaseDetails(Transition transition)
+        {
+
+        }
+
+        public Transition GetCaseDetails(string recordNumber)
+        {
+            Transition transition = new Transition();
+            return transition;
+        }
+
+        public Transition ExecuteTransition(Transition transition)
+        {
+            return transition;
+        }
+
+        public List<Transition> GetPossibleTransitions(Transition transition)
+        {
+            List<Transition> transitions = null;
+            transitions = Transitions.FindAll(tran => tran.CurrentStatus == transition.CurrentStatus && tran.CurrentPhase == transition.CurrentPhase);
+            if (transitions == null)
+            {
+                throw new Exception("Need to implement custom workflow exceptions");
+            }
+
+            return transitions;
+        }
+
+        public StartCaseOutput StartCase(StartCaseInput caseInput)
+        {
+
+            //ToDo: Validations
+
+            StartCaseOutput caseOutput = new StartCaseOutput();
+
+            try
+            {
+                List<Transition> transitions = GetPossibleTransitions(new Transition()
+                {
+                    CurrentPhase = string.Empty,
+                    CurrentStatus = string.Empty,
+                    Action = caseInput.ACP ? "NewACP" : "NewNonACP"
+                });
+
+                UpdateCaseDetailsInDb();
+
+                caseOutput.Success = true;
+            }
+            catch (Exception ex)
+            {
+                caseOutput.Success = false;
+                caseOutput.ErrorMessage = ex.Message;
+            }
+            return caseOutput;
+        }
+
+        public ClaimCaseOutput ClaimCase(ClaimCaseInput caseInput)
+        {
+            ClaimCaseOutput caseOutput = new ClaimCaseOutput();
+
+            try
+            {
+                List<Transition> transitions = GetPossibleTransitions(new Transition()
+                {
+                    CurrentPhase = "Intake",
+                    CurrentStatus = caseInput.ACP ? "Pending Assignment" : "Unassigned",
+                    Action = "Claim"
+                });
+
+                UpdateCaseDetailsInDb();
+
+                caseOutput.Success = true;
+
+                return caseOutput;
+            }
+            catch (Exception ex)
+            {
+                caseOutput.Success = false;
+                caseOutput.ErrorMessage = ex.Message;
+            }
+            return caseOutput;
+        }
+
+        public TransferCaseOutput TransferCase(TransferCaseInput caseInput)
+        {
+            TransferCaseOutput caseOutput = new TransferCaseOutput();
+
+            //Todo: Validations
+
+            UpdateCaseTransferDetails();
+
+            return caseOutput;
+        }
+
+        private void UpdateCaseTransferDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateCaseDetailsInDb()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WorkflowInput
+    {
+        public int Actor { get; set; }
+        public string WorkflowType { get; set; }
+        public int RecordNumber { get; set; }
+
+    }
+
+    public class WorkflowOutput
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class StartCaseInput : WorkflowInput
+    {
+        public bool ACP { get; set; }
+    }
+
+    public class ClaimCaseInput : WorkflowInput
+    {
+        public bool ACP { get; set; }
+
+    }
+
+    public class TransferCaseInput : WorkflowInput
+    {
+        public bool ACP { get; set; }
+    }
+
+    public class TransferCaseOutput : WorkflowOutput
+    {
+
+    }
+
+
+    public class StartCaseOutput : WorkflowOutput
+    {
+
+    }
+
+    public class ClaimCaseOutput : WorkflowOutput
+    {
+    }
+}
